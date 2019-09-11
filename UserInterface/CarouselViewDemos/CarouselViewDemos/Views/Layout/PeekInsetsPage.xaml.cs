8a01f86fde2e86055890d7b29d970e0955ca3dc3
Paul DiPietro <pauldipietro@users.noreply.github.com>
2019-09-11 14:37:27 -0400

    Add basic CarouselView samples

---
+++ UserInterface/CarouselViewDemos/CarouselViewDemos/Views/Layout/PeekInsetsPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using CarouselViewDemos.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarouselViewDemos.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PeekInsetsPage : ContentPage
+    {
+        public PeekInsetsPage()
+        {
+            InitializeComponent();
+            BindingContext = new MonkeysViewModel();
+        }
+    }
+}
